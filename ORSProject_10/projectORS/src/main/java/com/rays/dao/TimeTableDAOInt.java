35cd605f2f931af87572da99cd97b025f3c67fc1
Your Name <you@example.com>
2024-09-06 18:45:35 +0530

    java

---
+++ ORSProject_10/projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * @author Prashant Ojha 
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
